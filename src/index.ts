fa0cb7824066a1bcead17e15592e4d1d861e0b4a
David Severwright <code@davidsev.co.uk>
2023-09-04 23:04:13 +0100

    export the cell stuff

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import OBR from '@owlbear-rodeo/sdk';
 import { Grid } from './Grid';
 
 export { Point } from './Point';
+export { Cell } from './Grid/Cell/Cell';
+export { Square } from './Grid/Cell/Square';
+export { VHex } from './Grid/Cell/VHex';
+export { HHex } from './Grid/Cell/HHex';
 
 export const grid = Grid.getInstance();
 export { Grid };
