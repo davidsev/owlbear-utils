8478dc7532f2077b912fe826c03fb82fb33172c2
David Severwright <code@davidsev.co.uk>
2023-09-30 00:51:30 +0100

    added item metadata

--- src/Metadata/Base.ts
+++ src/Metadata/Base.ts
@@ -1,6 +1,18 @@
 import { Metadata } from '@owlbear-rodeo/sdk';
 import { awaitReady } from '../awaitReady';
 
+export function cleanMetadata<T> (values: Metadata, defaultValues: T): T {
+    // Start with the default values.
+    const clean = { ...defaultValues };
+
+    // If any of the values are valid, copy them over.
+    for (const key in defaultValues)
+        if (values[key] !== undefined && values[key] !== null && typeof (values[key]) === typeof (defaultValues[key]))
+            (clean[key] as any) = values[key];
+
+    return clean;
+}
+
 export abstract class BaseMetadataMapper<T> {
 
     constructor (
@@ -9,15 +21,7 @@ export abstract class BaseMetadataMapper<T> {
     ) { }
 
     public clean (values: Metadata): T {
-        // Start with the default values.
-        const clean = { ...this.defaultValues };
-
-        // If any of the values are valid, copy them over.
-        for (const key in this.defaultValues)
-            if (values[key] !== undefined && values[key] !== null && typeof (values[key]) === typeof (this.defaultValues[key]))
-                (clean[key] as any) = values[key];
-
-        return clean;
+        return cleanMetadata(values, this.defaultValues);
     }
 
     protected abstract getRawMetadata (): Promise<Metadata>;
