f82b45ba13b24882d149a5d43ced41a8017b6841
David Severwright <code@davidsev.co.uk>
2023-10-14 23:58:22 +0100

    added comment

--- src/Grid/Cell/BaseHex.ts
+++ src/Grid/Cell/BaseHex.ts
@@ -20,14 +20,14 @@ export abstract class BaseHex extends Cell {
         const y1 = nearestCorner.y;
         const x2 = secondNearestCorner.x;
         const y2 = secondNearestCorner.y;
-        
+
         // Line 2 is point to center
         const x3 = point.x;
         const y3 = point.y;
         const x4 = this.center.x;
         const y4 = this.center.y;
 
-        // Find where they intersect.
+        // Find where they intersect.  (https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection)
         return new Point({
             x: ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)),
             y: ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)),
