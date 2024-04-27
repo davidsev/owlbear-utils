1836deb789fb7bfefc688ef69c5222e572ef99e9
David Severwright <code@davidsev.co.uk>
2024-04-27 21:44:20 +0100

    make cleanMetadata accept undefined and null for the values

--- src/Metadata/cleanMetadata.ts
+++ src/Metadata/cleanMetadata.ts
@@ -1,13 +1,14 @@
 import { Metadata } from '@owlbear-rodeo/sdk';
 
-export function cleanMetadata<T> (values: Metadata, defaultValues: T): T {
+export function cleanMetadata<T> (values: Metadata | null | undefined, defaultValues: T): T {
     // Start with the default values.
     const clean = { ...defaultValues };
 
     // If any of the values are valid, copy them over.
-    for (const key in defaultValues)
-        if (values[key] !== undefined && values[key] !== null)
-            (clean[key] as any) = values[key];
+    if (values)
+        for (const key in defaultValues)
+            if (values[key] !== undefined && values[key] !== null)
+                (clean[key] as any) = values[key];
 
     return clean;
 }
