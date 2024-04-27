9e6491f9435d1f3c51c79dcbcad7134329c5cc88
David Severwright <code@davidsev.co.uk>
2024-04-27 21:33:52 +0100

    added more values to SnapTo so type checking works as expected

--- src/Grid/SnapTo.ts
+++ src/Grid/SnapTo.ts
@@ -1,6 +1,10 @@
 export enum SnapTo {
-    CORNER = 1 << 1,
-    CENTER = 1 << 2,
-    EDGE = 1 << 3,
+    CORNER = 1 << 0,
+    CENTER = 1 << 1,
+    EDGE = 1 << 2,
+    CORNER_AND_CENTER = SnapTo.CORNER | SnapTo.CENTER,
+    CORNER_AND_EDGE = SnapTo.CORNER | SnapTo.EDGE,
+    CENTER_AND_EDGE = SnapTo.CENTER | SnapTo.EDGE,
+    CORNER_AND_CENTER_AND_EDGE = SnapTo.CORNER | SnapTo.CENTER | SnapTo.EDGE,
     ALL = -1,
 }
