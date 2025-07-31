295c5b343a06f73b68ef5d4c67c17cd7a749f159
David Severwright <code@davidsev.co.uk>
2025-07-31 20:02:57 +0100

    removed dead tests

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: { '^.+\\.[tj]s$': 'ts-jest' },
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.test\\.ts$',
-    moduleFileExtensions: ['ts', 'js'],
-    transformIgnorePatterns: ['node_modules/(?!@owlbear-rodeo)/'],
-};
